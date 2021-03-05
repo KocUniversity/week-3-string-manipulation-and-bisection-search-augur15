9be90c67c70bf4bba57a1e6b47cc0051c5ec8c59
augur15 <augur15@ku.edu.tr>
2021-03-05 11:47:44 +0000

    solutions

--- solutions.py
+++ solutions.py
@@ -4,8 +4,18 @@
 s1 = "BAAABBAABABA"
 s2 = ""
 counter = 0
-
-
+for i in range(len(s1)):
+  if i == 0:
+    prev = s1[i]
+    s2 += prev #s1[i]
+  else:
+    curr = s1[i]
+    if curr == prev:
+      counter += 1
+    else:
+      prev = curr
+      s2 += curr  
+ 
 print("My original string was", s1)
 print("My manipulated string is", s2)
 print("The manipulation required", counter, "deletions")
@@ -16,6 +26,22 @@ s1 = 'abccea'
 s2 = 'abcdea'
 smaller = -1
 i = 0
+while i < len(s1) and i < len(s2):
+  if s1[i] < s2[i]:
+    smaller = s1
+    break
+  elif s2[i] < s1[i]:
+    smaller = s2
+    break
+  else:
+    i += 1
+
+if i == len(s1) and i == len(s2):
+  smaller = -1
+elif i == len(s2):
+  smaller = s2
+elif i == len(s1):
+  smaller = s1
 
 
 print('smaller string is', smaller)
@@ -26,7 +52,17 @@ s1 = 'aaaaaaa'
 s2 = 'bbbbcccdddd'
 out = ''
 i = 0
+while i < len(s1) and i < len(s2:)
+  out += s1[i] + s2[i]
+  i += 1
+while i < len(s1):
+  out += s1[i]
+  i += 1
+while i < len(s2):
+  out += s2[i]
+  i += 1
 
+return out
 
 print(out)
 
@@ -44,6 +80,22 @@ print('This needs to be negative f_a =', f_a)
 print('This needs to be positive f_b =', f_b)
 
 i = 0
+while i < 10000:
+  c = (a + b) / 2
+
+  f_c = c ** 3 - c -2
+
+  if c - a < eps or abs(f_c) < eps:
+    print('solution is found at c =', c)
+    print('after i = ', i)
+    break
+
+  i += 1
+
+  if f_c < 0:
+    a = c
+  else:
+    b = c    
 
 
 if i == max_iter:
